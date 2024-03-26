d46e0e78e59c6664f67d1173231e0d881a13fbf2
HasanDukanwala <dukanwalahasan@gmail.com>
2024-03-26 16:31:02 -0400

    Made first ajax call with DOM

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -3,3 +3,15 @@ console.log('Hello World')
 const helloWorldBox = document.getElementById('hello-world')
 
 helloWorldBox.textContent = 'Hello World'
+
+$.ajax({
+    type: 'Get',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response)
+        helloWorldBox.textContent=response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
