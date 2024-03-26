f666cb473de41b666d69e119e74e9849cfd698b9
HasanDukanwala <dukanwalahasan@gmail.com>
2024-03-26 12:44:07 -0400

    Figured out Typo that was leading to strange errors and not let server run.

--- posts_proj/urls.py
+++ posts_proj/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/admin/', admin.site.urls),
 ]
 
-urlpatterns+= static(settings.STATIC_URL, document_root= static(settings.STATIC_ROOT))
-urlpatterns+= static(settings.MEDIA_URL, document_root= static(settings.MEDIA_ROOT))
+urlpatterns+= static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
