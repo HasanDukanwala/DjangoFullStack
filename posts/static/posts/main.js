129715aee40ec9584abfcb936bfc84a2d6925595
HasanDukanwala <dukanwalahasan@gmail.com>
2024-03-26 17:58:05 -0400

    Added foreach lop to append data into empty data list in main.js

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('Hello World')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postBox = document.getElementById('posts-box')
 
 helloWorldBox.textContent = 'Hello World'
 
@@ -21,6 +22,13 @@ $.ajax({
     url: '/data/',
     success: function(response){
         console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `
+        });
     },
     error: function(error){
         console.log(error)
