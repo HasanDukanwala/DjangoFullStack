c384f051fccbb8fbdea8e792412aba55afa71b3b
HasanDukanwala <dukanwalahasan@gmail.com>
2024-03-28 12:49:23 -0400

    Made update box function and it now updates post with new title and body etc.

--- posts/views.py
+++ posts/views.py
@@ -96,13 +96,13 @@ def update_post(request,pk):
         obj.body = new_body
         obj.save()
 
-    return JsonResponse({
-        'title': new_title,
-        'body': new_body,
-    })
+        return JsonResponse({
+            'title': new_title,
+            'body': new_body,
+        })
 
 def delete_post(request,pk):
     obj = Post.objects.get(pk=pk)
     if request.method == 'POST':
         obj.delete()
-    return JsonResponse({})
+        return JsonResponse({})
